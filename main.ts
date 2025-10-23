554002cc3fba0dc3576d7c1bc5a0ab4a9504b3d4
Xander-Henderson <xander.henderson@stu.ocsb.ca>
2025-10-23 12:52:38 -0400

    added code

--- main.ts
+++ main.ts
@@ -19,4 +19,28 @@ xandersNeopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
 xandersNeopixelStrip.show()
 basic.showIcon(IconNames.Happy)
 
+input.onButtonPressed(Button.A, function () {
+    // gets distance to object in cm
+    basic.clearScreen()
+    xandersNeopixelStrip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
+    xandersNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    xandersNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    xandersNeopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    xandersNeopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    xandersNeopixelStrip.show()
+    distanceToObjest = sonar.ping(DigitalPin.P0, DigitalPin.P0, PingUnit.Centimeters)
 
+    if (distanceToObjest < 10) {
+        xandersNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        xandersNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        xandersNeopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        xandersNeopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        xandersNeopixelStrip.show()
+    } else {
+        xandersNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        xandersNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+        xandersNeopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+        xandersNeopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+        xandersNeopixelStrip.show()
+    }
+})
