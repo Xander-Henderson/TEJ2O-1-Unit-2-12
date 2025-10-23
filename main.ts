915cc96380b0b59849c0ebc3d2eb19a4e0baa6ca
Xander-Henderson <xander.henderson@stu.ocsb.ca>
2025-10-23 12:36:08 -0400

    basic setup

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
+ * Created by: Alexander
+ * Created on: Oct 2025
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceToObjest: number = 0
+let xandersNeopixelStrip: neopixel.Strip = null
+
+// setup
+basic.clearScreen()
+xandersNeopixelStrip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
+xandersNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+xandersNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+xandersNeopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+xandersNeopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+xandersNeopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+
